74261230acfacf60ff6fc6528af0f972fb524be9
arun-kumar19 <arunmaurya1177@gmail.com>
2023-06-22 00:15:16 +0530

    experss framework demo

---
+++ EXPRESS JS/express js demo/app.js
@@ -0,0 +1,22 @@
+const express=require('express');
+
+
+
+const app=express();
+
+app.use((req,res,next)=>{
+
+    console.log("In the middleware");
+    next();
+
+})
+
+
+app.use((req,res,next)=>{
+
+    console.log("In another middleware");
+    res.send('<h1>Hello from Express!');
+
+})
+
+app.listen(4000);
