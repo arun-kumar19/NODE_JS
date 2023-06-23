041fdc078275adbb9399932021c8c0e76fa0192f
arun-kumar19 <arunmaurya1177@gmail.com>
2023-06-23 09:54:09 +0530

    middleware

--- EXPRESS JS/express js demo/app.js
+++ EXPRESS JS/express js demo/app.js
@@ -1,22 +1,29 @@
 const express=require('express');
-
-
+const bodyParser=require('body-parser');
 
 const app=express();
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.use((req,res,next)=>{
+app.use('/add-product',(req,res,next)=>{
 
-    console.log("In the middleware");
-    next();
+    const htmlData=`<html><body><form action="product" method="POST"><input type="text" name="title"><br>
+    <input type="text" name="size"><br>
+    <button>Add Product</button></form><body><html>`
+    res.send(htmlData);
+})
+
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    const obj=req.body;
+    console.log("title="+obj.title);
+    console.log("size="+obj.size);
+    res.redirect('/');
+})
+
+app.use('/',(req,res,next)=>{
+
+    res.send('<html><body><h1>Hello from Express JS!</h1></form>');
 
 })
 
-
-app.use((req,res,next)=>{
-
-    console.log("In another middleware");
-    res.send('<h1>Hello from Express!');
-
-})
-
-app.listen(4000);
+app.listen(3000);
