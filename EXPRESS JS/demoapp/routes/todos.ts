bf20232a03dbf4476ee7a1c6d5edd9efa070141d
arun-kumar19 <arunmaurya1177@gmail.com>
2023-12-24 13:13:23 +0530

    typescript2

--- EXPRESS JS/demoapp/routes/todos.ts
+++ EXPRESS JS/demoapp/routes/todos.ts
@@ -4,7 +4,10 @@ import {Todo} from '../models/todo';
 
 const router=Router();
 
-const todos: Todo[]=[]
+let todos: Todo[]=[]
+
+type  requestBody={text:string};
+type  requestParam={todoid:string}
 
 router.get('/',(req,res,next)=>{
 
@@ -13,20 +16,22 @@ router.get('/',(req,res,next)=>{
 
 router.post('/todo',(req,res,next)=>{
     console.log('request data=',req.body.text);
-    console.log('request data=',req.body);
+    
+    const request=req.body as requestBody;
+    const data=request.text
     const newTodo:Todo={
         id: new Date().toISOString(),
-        text:req.body.text,
+        text:data,
     }
     todos.push(newTodo);
     res.status(200).json({success:1});
 })
 
-router.post('/deletetodo',(req,res,next)=>{
-
-    const user=req.body.text;
-    console.log('itemid=',user);
-    const result = deleteByIdInPlace(user);
+router.delete('/deletetodo/:todoid',(req,res,next)=>{
+    const params=req.params as requestParam;
+    const todoid=params.todoid;
+    console.log('todoid=',todoid);
+    const result = deleteByIdInPlace(todoid);
     console.log('result=',result);
 if (result) {
   res.status(200).json({'Found:':'item deleted'});
@@ -38,7 +43,7 @@ if (result) {
 })
 
 function deleteByIdInPlace(id: string): boolean {
-    const index = todos.findIndex(obj => obj.text === id);
+    const index = todos.findIndex(obj => obj.id === id);
     if (index !== -1) {
       todos.splice(index, 1);
       return true;
@@ -49,13 +54,12 @@ function deleteByIdInPlace(id: string): boolean {
 }
 
 
-router.post('/edittodo',(req,res,next)=>{
-
-    const user=req.body.oldusername;
-    const newuser=req.body.newusername;
-
-    console.log('itemid=',user);
-    const result = editByNameInPlace(user,newuser);
+router.put('/edittodo/:todoid',(req,res,next)=>{
+    const params=req.params as requestParam;
+    const todoid=params.todoid;
+    const request=req.body as requestBody;
+    const data=request.text;
+    const result = editByNameInPlace(todoid,data);
     console.log('result=',result);
 if (result) {
   res.status(200).json({'Found:':'Name updated'});
@@ -68,7 +72,7 @@ if (result) {
 
 
 function editByNameInPlace(id: string,newuser:string): boolean {
-    const index = todos.findIndex(obj => obj.text === id);
+    const index = todos.findIndex(obj => obj.id === id);
     if (index !== -1) {
       const todo:Todo=todos[index];
       todo.text=newuser;
